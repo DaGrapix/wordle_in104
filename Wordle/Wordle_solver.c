d5dc34df4430d8243cd0b1fc5e7333e40d9025d7
DaGrapix <theluffy62@outlook.fr>
2022-04-19 16:36:48 +0200

    A suivre

--- Wordle/Wordle_solver.c
+++ Wordle/Wordle_solver.c
@@ -10,7 +10,7 @@ char* dec_to_ternary(int number, int word_size){
     for (int i = 0; i <= word_size - 1; i++){
         configuration[i]='X';
     }
-    //return(configuration);
+
     int remainder = number;
     int power = 0;
     while (remainder != 0){
@@ -29,52 +29,6 @@ char* dec_to_ternary(int number, int word_size){
         }
     }
     return(configuration);
-}
-
-
-
-char* best_word(int word_size, char** list, int list_size, char** actual_list, int actual_list_size){
-    double probability;
-    double information;
-    double* entropy_list[list_size + 1];
-    char word[word_size + 1];
-    char word_state[word_size + 1];
-
-    for (int i = 0; i<= list_size - 1; i++){
-        word = list[i];
-        entropy_list[i] = entropy(word, actual_list_size, actual_list, word_size);
-        //printf("%f\n", (float)(i)/list_size);
-    }
-
-    double max_entropy=entropy_list[0];
-    char best_guess[word_size + 1] = list[0];
-    for (int i = 0; i<= list_size - 1; i++){
-        if (entropy_list[i] > max_entropy){
-            max_entropy = entropy_list[i];
-            best_guess = list[i];
-        }
-    }
-    return(best_guess);
-}
-
-
-double entropy(char* word, int actual_list_size, char** actual_list, int word_size){
-    int occurence = 0;
-    double probability = 0;
-    double entropy = 0;
-    char temp_word[word_size + 1];
-    for (int j = 0; j <= pow(3, word_size)-1; j++){
-        word_state = dec_to_ternary(j, word_size);
-        for (int i = 0; i <= actual_list_size - 1; i++){
-            temp_word=actual_list[i];
-            if (is_similar(word, word_state, temp_word, word_size)==true){
-                occurence++;
-            }
-        }
-        probability=occurence/actual_list_size;
-        entropy = entropy + probability*log(1/probability)/log(2);
-    }
-    return(entropy);
 }
 
 bool is_similar(char* word, char* word_state, char* list_word, int word_size){
@@ -129,4 +83,51 @@ bool is_similar(char* word, char* word_state, char* list_word, int word_size){
     }
 }
 
-*/
+double entropy(char* word, int actual_list_size, char** actual_list, int word_size){
+    int occurence = 0;
+    double probability = 0;
+    double entropy = 0;
+    char *temp_word;
+    for (int j = 0; j <= pow(3, word_size)-1; j++){
+        char* word_state = dec_to_ternary(j, word_size);
+        for (int i = 0; i <= actual_list_size - 1; i++){
+            temp_word=actual_list[i];
+            if (is_similar(word, word_state, temp_word, word_size)==true){
+                occurence++;
+            }
+        }
+        probability=occurence/actual_list_size;
+        entropy = entropy + probability*log(1/probability)/log(2);
+    }
+    return(entropy);
+}
+
+char* best_word(int word_size, char** list, int list_size, char** actual_list, int actual_list_size){
+    double probability;
+    double information;
+    double* entropy_list = malloc(list_size*sizeof(double));
+    char *word;
+    char *word_state;
+
+    //Creation de la liste des entropies de chaque mot
+    for (int i = 0; i<= list_size - 1; i++){
+        word = list[i];
+        entropy_list[i] = entropy(word, actual_list_size, actual_list, word_size);
+        //printf("%f\n", (float)(i)/list_size);
+    }
+
+    //On trouve le mot avec la plus grande entropie
+    double max_entropy=entropy_list[0];
+    char *best_guess;
+    strcpy(best_guess, list[0]);
+    for (int i = 0; i<= list_size - 1; i++){
+        if (entropy_list[i] > max_entropy){
+            max_entropy = entropy_list[i];
+            strcpy(best_guess, list[i]);
+        }
+    }
+    free(entropy_list);
+    return(best_guess);
+}
+
+
--- Wordle/test_similar.c
+++ Wordle/test_similar.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include <math.h>
+#include <time.h>
+
+#include "word_compare.h"
+#include "load_dic.h"
+#include "random_word.h"
+
 #include "Wordle_solver.h"
 
 int main(){
@@ -13,14 +19,26 @@ int main(){
     bool OK = is_similar(word,arrangement,list_word,5);
     printf("%d\n", OK);
     
-    */
+    
     for (int i = 0; i <= (int)pow(3,5) - 1; i++){
         char* configuration = dec_to_ternary(i, 5);
         printf("%s\n", configuration);
     }
+    */
+
 
     /*
     char* config = dec_to_ternary(5,5);
     printf("%s", config);
     */
+
+
+    srand(time(NULL));
+    char dico_name[8]="ods4.txt";
+    int word_length = 5;
+    int size;
+    char** list = read_dico(dico_name, &size, word_length);
+    char* bestWord = best_word(5, list, size, list, size);
+
+    printf("%s\n", bestWord);
 }

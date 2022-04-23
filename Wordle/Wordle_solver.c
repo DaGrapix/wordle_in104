0a448747d515d4fcc4bd9b6641df40ad210a4db2
DaGrapix <theluffy62@outlook.fr>
2022-04-23 12:18:07 +0200

    seg fault

--- Wordle/Wordle_solver.c
+++ Wordle/Wordle_solver.c
@@ -96,7 +96,7 @@ double entropy(char* word, int actual_list_size, char** actual_list, int word_si
                 occurence++;
             }
         }
-        probability=(double)occurence/actual_list_size;
+        probability=((double)occurence)/actual_list_size;
         entropy = entropy + probability*(log(1/probability)/log(2));
     }
     return(entropy);

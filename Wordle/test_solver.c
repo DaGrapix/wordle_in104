ed41e29b83f14c6b49dd0f2b9db011d4c5beaed1
DaGrapix <theluffy62@outlook.fr>
2022-05-03 18:31:23 +0200

    e

--- Wordle/test_solver.c
+++ Wordle/test_solver.c
@@ -11,25 +11,13 @@
 
 #include "Wordle_solver.h"
 
-int main(){
+int main(){    
+    //Test configuration
     /*
-    char list_word[6]="PARIS";
-    char word[6]="RREEP";
-    char arrangement[6]="ZXXXZ";
-    bool OK = is_similar(word,arrangement,list_word,5);
-    printf("%d\n", OK);
-    
-    
     for (int i = 0; i <= (int)pow(3,5) - 1; i++){
         char* configuration = dec_to_ternary(i, 5);
         printf("%s\n", configuration);
     }
-    */
-
-
-    /*
-    char* config = dec_to_ternary(5,5);
-    printf("%s", config);
     */
 
 
